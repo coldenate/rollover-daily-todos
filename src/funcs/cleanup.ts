7961a579caa71fc17c9f51b7a20800ee681d512f
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2026-02-09 16:20:48 -0600

    Fixes #31

--- src/funcs/cleanup.ts
+++ src/funcs/cleanup.ts
@@ -3,9 +3,11 @@ import { BuiltInPowerupCodes, ReactRNPlugin } from '@remnote/plugin-sdk';
 export async function cleanupPastDocuments(plugin: ReactRNPlugin) {
 	const rolledPowerup = await plugin.powerup.getPowerupByCode('rolled');
 	const rolledOverRems = await rolledPowerup?.taggedRem();
-	const isNotPortalMode = await plugin.settings.getSetting('portal-mode');
+	const portalMode = await plugin.settings.getSetting('portal-mode');
 
-	if (!rolledOverRems || !isNotPortalMode) {
+	// In portal mode, rolled portals should remain in past daily docs so completed
+	// items can still be reviewed historically.
+	if (!rolledOverRems || portalMode) {
 		return;
 	}
 
